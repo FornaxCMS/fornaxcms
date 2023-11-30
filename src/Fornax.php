3c88b729e76c18d6d00b639e6b0b63eddbfe8783
ishakdeveloper <ishak.development@outlook.com>
2023-11-30 13:47:25 +0100

    tag

--- src/Fornax.php
+++ src/Fornax.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Http;
 class Fornax {
     public function getFromHttp() {
         $response = Http::get('https://inspiration.goprogram.ai/');
-
         return $response['quote'] . ' -' . $response['author'];
     }
 }
